fa49fc064ca9f849df6732e0c9bb1afe2ef2b2a0
0neGuyDev <0neguybusiness@gmail.com>
2020-06-19 19:45:37 +0200

    It does stuff now :p

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
 /* Copyrighted by 0neGuyDev */
 
+document.addEventListener("keyup", (e) => {
+	updateText();
+})
+
+function updateText() {
+	if (notEmpty(bigTitleInput.value)) {
+		bigTitle.innerHTML = bigTitleInput.value
+	} else {
+		bigTitle.innerHTML = "Big Title"
+	}
+	
+	if (notEmpty(subTitleInput.value)) {
+		subTitle.innerHTML = subTitleInput.value
+	} else {
+		subTitle.innerHTML = "Sub Title"
+	}
+	
+	if (notEmpty(subTextInput.value)) {
+		subText.innerHTML = subTextInput.value
+	} else {
+		subText.innerHTML = "Sub Text"
+	}
+}
+
+function notEmpty(variable) {
+	if (variable.replace(/ /g, "") !== "") {
+		return true
+	} else {
+		return false
+	}
+}
