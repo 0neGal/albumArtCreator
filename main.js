df85d8541d3411ab6a6ffc898a1449abadb49511
0neGuyDev <0neguybusiness@gmail.com>
2020-06-20 23:01:06 +0200

    cleany clean...
    yay it clean

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ document.addEventListener("keyup", (e) => {
 });
 
 document.addEventListener('touchmove', function (event) {
-	if (event.scale !== 1) { event.preventDefault(); }
+	if (event.scale !== 1) {event.preventDefault();}
 }, { passive: false });
 
 function updateText() {
-	subText.innerHTML = subTextInput.value
-	bigTitle.innerHTML = bigTitleInput.value
-	subTitle.innerHTML = subTitleInput.value
+	subText.innerHTML = subTextInput.value;
+	bigTitle.innerHTML = bigTitleInput.value;
+	subTitle.innerHTML = subTitleInput.value;
 }
 
 function generate() {
@@ -23,9 +23,9 @@ function toggleModal() {
 }
 
 function randomColor() {
-	let colors = ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8"]
-	setVariable("activecolor", colors[Math.floor(Math.random() * colors.length)])
-}; randomColor()
+	let colors = ["#55efc4", "#81ecec", "#74b9ff", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8"];
+	setVariable("activecolor", colors[Math.floor(Math.random() * colors.length)]);
+}; randomColor();
 
 function setVariable(variable, value) {
 	document.getElementsByTagName('html')[0].style.cssText = `--${variable}: ${value}`;
